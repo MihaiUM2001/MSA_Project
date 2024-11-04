e13f7e28fcd6a24a858f43ba6342c029d2b4fb33
MihaiUM2001 <mihai-alexandru.udrescu-milosav@student.upt.ro>
2024-11-04 23:03:17 +0200

    Started backend

---
+++ src/main/java/com/example/swappy/SwappyApplication.java
@@ -0,0 +1,13 @@
+package com.example.swappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwappyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwappyApplication.class, args);
+    }
+
+}
