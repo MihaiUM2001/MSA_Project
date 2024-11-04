8fa1d694912baee6664568a67ccdc31f4e3f806a
MihaiUM2001 <mihai-alexandru.udrescu-milosav@student.upt.ro>
2024-11-04 23:03:17 +0200

    Started backend

---
+++ src/main/java/com/example/swappy/model/User.java
@@ -0,0 +1,37 @@
+package com.example.swappy.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String profilePictureURL;
+
+    @OneToMany(mappedBy = "seller", cascade = CascadeType.ALL)
+    private List<Product> products;
+
+    @OneToMany(mappedBy = "buyer", cascade = CascadeType.ALL)
+    private List<Swap> swapsAsBuyer;
+
+    @OneToMany(mappedBy = "seller", cascade = CascadeType.ALL)
+    private List<Swap> swapsAsSeller;
+
+    // Getters and setters
+}
