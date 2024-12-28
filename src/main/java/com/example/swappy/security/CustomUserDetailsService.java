9fb49e563580cc3ac8abad352564a8b309426f9c
xatyy <anthony.esber@student.upt.ro>
2024-12-28 18:49:11 +0200

    Backend | Improved login logic with meaningful exceptions on failure

--- src/main/java/com/example/swappy/security/CustomUserDetailsService.java
+++ src/main/java/com/example/swappy/security/CustomUserDetailsService.java
@@ -20,16 +20,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-
         User user = userRepository.findByEmail(username);
-
-
 
         if (user == null) {
             throw new UsernameNotFoundException("User not found");
         }
-
-        System.out.println("Password from DB: " + user.getPassword());
 
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
     }
